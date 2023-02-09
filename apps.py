7aaf1def22a7be2eaed94317e57221250c539b4e
hellofromtheothersky <>
2023-02-09 11:13:06 +0700

    first commit
    first commit

---
+++ apps.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from flask import request
+from flask import redirect, url_for
+from flask import session
+from flask import jsonify
+from relation_extraction.process_data import RE_DataEncoder
+import relation_extraction.predict as re_model
+
+app = Flask(__name__)
+app.secret_key = b"abc5b2db26e723e327dd0b30c377fb6ec709973aa3184b750e4e3e32adc22f9e"
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    global sentence
+    sentence_token = None
+    if request.method == "POST":
+        s = request.form["sentence"]
+        if s:
+            sentence = s
+            sentence_token = re_model.sentence_token(sentence)
+    return render_template("home.html", sentence_token=sentence_token)
+
+
+@app.route("/predict")
+def predict():
+    e1pos = request.args.getlist("e1pos[]")
+    e2pos = request.args.getlist("e2pos[]")
+    result = re_model.predict(sentence, e1pos, e2pos)
+    return jsonify(
+        {"e1": result[0], "type": result[1], "e2": result[2], "acc": result[3]}
+    )
+    # return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    sentence = ""
+    app.run(debug=True)
---
+++ relation_extraction/predict.py
@@ -0,0 +1,78 @@
+import re
+import pickle
+from relation_extraction.process_data import get_feature, RE_DataEncoder
+from keras.models import model_from_json
+
+
+def load_model(model_name):
+    # path='/content/drive/MyDrive/nlp/relation_extraction/saved_model/'
+    path = "relation_extraction/saved_model/"
+    with open(path + model_name + ".json", "r") as rf:
+        jstr = rf.read()
+    model = model_from_json(jstr)
+    model.load_weights(path + model_name + ".h5")
+    return model
+
+
+def go_predict(model, input):
+    with open("relation_extraction/data_encoder.obj", "rb") as rf:
+        Encoder = pickle.load(rf)
+
+    sentences, e1_distance, e2_distance, grammar, sp = get_feature([input])
+    sentences_np, e1_dist_np, e2_dist_np, grammar_np, sp_np = Encoder.encode(
+        sentences, e1_distance, e2_distance, grammar, sp
+    )
+    pred = model.predict([sentences_np, e1_dist_np, e2_dist_np, grammar_np, sp_np])
+    acc=pred[0].max()
+    pred = pred.argmax(axis=1)
+    return Encoder.dict_labels[int(pred)], acc
+
+
+def sentence_token(text):
+    text = re.sub(r"[^\w ]", "", text)
+    text = re.sub(r"(\s+\s\s*|\s*\s\s+)", " ", text)
+    text = text.strip()
+    return text.split()
+
+
+def predict(text, e1pos, e2pos):
+    text = sentence_token(text)
+
+    e1pos = sorted(list(set([int(x) for x in e1pos])))
+    e2pos = sorted(list(set([int(x) for x in e2pos])))
+    print(e1pos, e2pos)
+    p1 = e1pos[0]
+    p2 = e1pos[-1] + 1
+    p3 = e2pos[0]
+    p4 = e2pos[-1] + 1
+    e1=" ".join(text[p1:p2])
+    e2=" ".join(text[p3:p4])
+    text = (
+        " ".join(text[:p1])
+        + "<e1>"
+        + " ".join(text[p1:p2])
+        + "</e1>"
+        + " ".join(text[p2:p3])
+        + "<e2>"
+        + " ".join(text[p3:p4])
+        + "</e2>"
+        + " ".join(text[p4:])
+    )
+    print(text)
+
+    label, acc = go_predict(load_model("cnn_nonbert"), text)
+    re_type=label.split('(')[0]
+    if 'e1,e2' in label:
+        return e1, re_type, e2, round(acc*100, 1)
+    else:
+        return e2, re_type, e1, round(acc*100, 1)
+
+
+if __name__ == "__main__":
+    print(
+        go_predict(
+            load_model("cnn_nonbert"),
+            '<e1>he</e1> go to <e2>bed</e2>',
+
+        )
+    )
